0f006cf49f69e358d9d8f333585eff9e7b42485f
Gmatieso <geoffrey.matieso@student.moringaschool.com>
2022-10-10 11:31:37 +0300

    Code refactored

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -6,12 +6,13 @@ function ArticleList({posts}) {
 
   // JS code to loop through blogData and display it in form of articles 
   // when using map to loop we must pass in an id, coz that is the syntax .
-    const  article = posts.map((blogs) => (
+    const  article = posts.map((post) => (
       <Article 
-      key = {blogs.id} 
-      title = {blogs.title}  
-      name = {blogs.name} 
-      preview = {blogs.preview} 
+      key = {post.id} 
+      title = {post.title} 
+      date = {post.date} 
+      minutes = {post.minutes}
+      preview = {post.preview} 
       />
   ))
 
